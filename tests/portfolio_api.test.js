b14ab9e09e3dddcbc3de12b43d2ea43214d2a983
Keith <keith.ong5000@gmail.com>
2020-03-31 12:08:20 -0700

    Refactor portfolio_api.test

--- tests/portfolio_api.test.js
+++ tests/portfolio_api.test.js
@@ -2,20 +2,41 @@ const User = require('../models/user')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
+const helper = require('./test_helper')
 
-const initialUser = {
-    username: 'testuser',
-    password: 'testpassword'
+let authorizationToken = null
+const getToken = async () => {
+    if (authorizationToken) {
+        return authorizationToken
+    } else {
+        return await logUserIn()
+    }
 }
 
-const testStock1 = {
-    "ticker": "IBM",
-    "name": "International Business Machines",
-    "shares": 5,
-    "price": 10
+const logUserIn = async () => {
+    const user = await api
+        .post('/api/login')
+        .send(helper.initialUser)
+        .expect(200)
+    authorizationToken = user.body.token
+    return authorizationToken
 }
 
-const addStock = async (stock, token) => {
+const createUser = async (user) => {
+    await api
+        .post('/api/users')
+        .send(helper.initialUser)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+}
+
+const deleteAllUsers = async () => {
+    await User.deleteMany({})
+}
+
+const addStock = async (stock) => {
+    const token = await getToken()
+
     await api
         .post('/api/portfolio/asset')
         .set('Authorization', 'Bearer ' + token)
@@ -24,87 +45,91 @@ const addStock = async (stock, token) => {
         .expect('Content-Type', /application\/json/)
 }
 
-const getTestUserStocks = async (token) => {
+const getTestUserAssets = async () => {
+    const token = await getToken()
+
     const response = await api
         .get('/api/portfolio')
         .set('Authorization', 'Bearer ' + token)
         .expect(200)
         .expect('Content-Type', /application\/json/)
-    console.log(response.body)
     return response.body
 }
 
-const sellStock = async (order, token) => {
-    const response = await api
+const getTestUserStocks = async () => {
+    const assets = await getTestUserAssets()
+    return assets.stocks
+}
+
+const getTestUserCash = async () => {
+    const assets = await getTestUserAssets()
+    return assets.cash
+}
+
+const sellStock = async (order) => {
+    const token = await getToken()
+
+    await api
         .post('/api/portfolio/sell')
         .set('Authorization', 'Bearer ' + token)
         .send(order)
         .expect(200)
         .expect('Content-Type', /application\/json/)
-    console.log(response.body.cash)
-    console.log(response.body.stocks)
 }
 
-let authorizationToken = null
-
 beforeEach(async () => {
-    await User.deleteMany({})
-
-    await api
-        .post('/api/users')
-        .send(initialUser)
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-
-    const user = await api
-        .post('/api/login')
-        .send(initialUser)
-        .expect(200)
-    authorizationToken = user.body.token
+    await deleteAllUsers()
+    await createUser(helper.initialUser)
+    await logUserIn()
 })
 
 describe('Adding stocks to portfolio', () => {
     test('Can add a new stock to the portfolio', async () => {
-        await addStock(testStock1, authorizationToken)
+        await addStock(helper.testStocks[0])
 
-        const userStocks = await getTestUserStocks(authorizationToken)
+        const userStocks = await getTestUserStocks()
         const stockTickers = userStocks.map(stock => stock.ticker)
-        expect(stockTickers).toContain(testStock1.ticker)
+        expect(stockTickers).toContain(helper.testStocks[0].ticker)
     })
 
     test('Adding a stock that already exist increases share count of existing entry', async () => {
-        await addStock(testStock1, authorizationToken)
-        await addStock(testStock1, authorizationToken)
+        await addStock(helper.testStocks[0])
+        await addStock(helper.testStocks[0])
 
-        const stocks = await getTestUserStocks(authorizationToken)
-        const updatedStock = stocks.find(stock => stock.ticker === testStock1.ticker)
-        expect(updatedStock.shares).toEqual(2 * testStock1.shares)
+        const stocks = await getTestUserStocks()
+        const updatedStock = stocks.find(stock => stock.ticker === helper.testStocks[0].ticker)
+        expect(updatedStock.shares).toEqual(2 * helper.testStocks[0].shares)
     })
 })
 
 describe('Selling stocks from portfolio', () => {
     test('Error on selling a stock user does not own', async () => {
+        const token = await getToken()
         await api
             .post('/api/portfolio/sell')
-            .set('Authorization', 'Bearer ' + authorizationToken)
-            .send(testStock1)
+            .set('Authorization', 'Bearer ' + token)
+            .send(helper.testStocks[0])
             .expect(401)
     })
 
     test('Selling a stock reduces share count and adds sold value to user cash', async () => {
-        await addStock(testStock1, authorizationToken)
+        const stock = helper.testStocks[0]
+        await addStock(stock)
 
         const sharesToSell = 1
         const price = 10
         const sellOrder = {
-            ticker: testStock1.ticker,
+            ticker: stock.ticker,
             shares: sharesToSell,
             price
         }
-        await sellStock(sellOrder, authorizationToken)
+        await sellStock(sellOrder)
 
-        const userStocks = await getTestUserStocks(authorizationToken)
-        const testStock = userStocks.find(stock => stock.ticker === testStock1.ticker)
-        expect(testStock.shares).toEqual(testStock1.shares - sharesToSell)
+        const userStocks = await getTestUserStocks()
+        const updatedStock = userStocks.find(stock => stock.ticker === stock.ticker)
+        expect(updatedStock.shares).toEqual(stock.shares - sharesToSell)
+
+        const cash = await getTestUserCash()
+        expect(cash).toEqual(sharesToSell * price)
     })
 })
---
+++ tests/test_helper.js
@@ -0,0 +1,17 @@
+const initialUser = {
+    username: 'testuser',
+    password: 'testpassword'
+}
+
+const testStocks = [
+    {
+        "ticker": "IBM",
+        "name": "International Business Machines",
+        "shares": 5,
+        "price": 10
+    }
+]
+
+module.exports = {
+    initialUser, testStocks
+}
